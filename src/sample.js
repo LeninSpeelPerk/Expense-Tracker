304d8728aa1dab09effcdc25c3595715deeb09ae
lenincena22 <lenincena22@gmail.com>
2022-05-30 13:13:08 +0530

    first commit

---
+++ src/sample.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+function Sample() {
+    return(
+        <>
+            <div className="deserts-list">
+                <div className="desserts" id="">
+                    <label></label>
+                    <img src="" alt="" />
+                </div>
+                <div className="desserts" id="">
+                    <label></label>
+                    <img src="" alt="" />
+                </div>
+                <div className="desserts" id="">
+                    <label></label>
+                    <img src="" alt="" />
+                </div>
+                <div className="desserts" id="">
+                    <label></label>
+                    <img src="" alt="" />
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Sample;
